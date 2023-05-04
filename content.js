01732d1ea3bcf4ba7a0923e40e586dac1dea5946
gcb <gcb@>
2023-05-04 15:22:15 +0200

    i guess we can be clumsy with this

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
-addEventListener("DOMContentLoaded", (event) => {
+//addEventListener("DOMContentLoaded", (event) => {
   document.body.innerHTML = document.body.innerHTML.replace(
-    /(\b(ai|gpt)\b|a\.i\.|artificial inteligence chatbot|artificial inteligence|chatgpt|GPT-3.5|GPT-4|GPT-4|GPT-2.5|GPT-2)/gmi,
+    /a\.i\.|artificial intelligence chatbots?|artificial intelligences?|chatgpt|GPT-3.5|GPT-4|GPT-4|GPT-2.5|GPT-2|(\b(ai|gpt|chatbot)\b)/gmi,
     'Clippy');
   document.body.innerHTML = document.body.innerHTML.replace(
     /(\b(nn|llm)\b|neural networks?|large language models?)/gmi,
@@ -8,5 +8,5 @@ addEventListener("DOMContentLoaded", (event) => {
   document.body.innerHTML = document.body.innerHTML.replace('OpenAI', 'Closed Source so-called-"Open"AI');
   // maybe AI -> 📎, NeuralNetwork -> 🖇️
   // TODO: i18n
-});
+//});
 
